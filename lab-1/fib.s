990c9a59b9e82a63db0272c40b5e3fe5f5423986
Wen.Liao <zzz00072@gmail.com>
2014-08-05 02:05:03 +0800

    lab-1: Replace tab to space

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -1,43 +1,43 @@
-	.syntax unified
-	.arch armv7-a
-	.text
-	.align 2
-	.thumb
-	.thumb_func
+    .syntax unified
+    .arch armv7-a
+    .text
+    .align 2
+    .thumb
+    .thumb_func
 
-	.global fibonacci
-	.type fibonacci, function
+    .global fibonacci
+    .type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+    @ ADD/MODIFY CODE BELOW
+    @ PROLOG
+    push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+    @ R4 = R0 - 0 (update flags)
+    @ if(R0 <= 0) goto .L3 (which returns 0)
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+    @ Compare R4 wtih 1
+    @ If R4 == 1 goto .L4 (which returns 1)
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+    @ R0 = R4 - 1
+    @ Recursive call to fibonacci with R4 - 1 as parameter
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+    @ R5 = R0
+    @ R0 = R4 - 2
+    @ Recursive call to fibonacci with R4 - 2 as parameter
 
-	@ R0 = R5 + R0 (update flags)
+    @ R0 = R5 + R0 (update flags)
 
-	pop {r3, r4, r5, pc}		@EPILOG
+    pop {r3, r4, r5, pc}        @EPILOG
 
-	@ END CODE MODIFICATION
+    @ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+    mov r0, #0            @ R0 = 0
+    pop {r3, r4, r5, pc}        @ EPILOG
 
 .L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+    mov r0, #1            @ R0 = 1
+    pop {r3, r4, r5, pc}        @ EPILOG
 
-	.size fibonacci, .-fibonacci
-	.end
+    .size fibonacci, .-fibonacci
+    .end
