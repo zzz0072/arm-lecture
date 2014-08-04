311bba59a33e4f595ad9e1f307b41ac934a2cfd7
Wen.Liao <zzz00072@gmail.com>
2014-08-05 02:24:25 +0800

    lab-1: Implement fibonacci sequence via comment hints

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -14,19 +14,33 @@ fibonacci:
     push {r3, r4, r5, lr}
 
     @ R4 = R0 - 0 (update flags)
+    sub r4, r0, 0
+
     @ if(R0 <= 0) goto .L3 (which returns 0)
+    cmp r0, 0
+    ble .L3
 
     @ Compare R4 wtih 1
     @ If R4 == 1 goto .L4 (which returns 1)
+    cmp r4, 1
+    beq .L4
 
     @ R0 = R4 - 1
     @ Recursive call to fibonacci with R4 - 1 as parameter
+    sub r0, r4, 1
+    bl  fibonacci
 
     @ R5 = R0
+    mov r5, r0
+
     @ R0 = R4 - 2
+    sub r0, r4, 2
+
     @ Recursive call to fibonacci with R4 - 2 as parameter
+    bl fibonacci
 
     @ R0 = R5 + R0 (update flags)
+    add r0, r0, r5
 
     pop {r3, r4, r5, pc}        @EPILOG
 
